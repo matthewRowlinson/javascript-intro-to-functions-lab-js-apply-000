e0638b2c92acd9285efcb08addd9c34080c6df4d
matthew rowlinson <matthewrowlinson1@gmail.com>
2020-01-28 20:12:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -35,8 +35,9 @@ function sayHiToGrandma(string){
     return "I can't hear you!";
   }
   else if (string == "I love you, Grandma."){
-    return "I love you, too."
+    return "I love you, too.";
   }
+  
 }
 
 ```
